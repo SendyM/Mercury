0d7ef5d826f02b9463daebff3f8faa0a11b0d93d
Sendy <daishihua@sanjiang365.com>
2018-05-18 17:55:13 +0800

    代码规范

--- src/main/java/com/mercury/MercuryApplication.java
+++ src/main/java/com/mercury/MercuryApplication.java
@@ -18,7 +18,7 @@ public class MercuryApplication
     public static void main(String[] args)
     {
         SpringApplication.run( MercuryApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |       \\  _. /  '       \n" +
